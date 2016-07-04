7c5bc6fb0f5958cb3629db1ead0c319873dca8d7
NicoleAgurcia <nicole_agurcia@unitec.edu>
2016-07-04 01:06:50 -0600

    updating api inventory,Insert and delete products

--- app/api/inventory.js
+++ app/api/inventory.js
@@ -15,16 +15,56 @@ const getProductData= (Product) => {
 
 api.getProducts = () => {
 	let promise  = new Promise( (resolve, reject) => {
-		 Product.fetchAll().then( Products => {
-		 		try {
-		 			resolve(new Response(1, Products.map(getProductData), "Successful Product Fecth"));
-		 		}
-		 		catch(err){
-		 			reject(new Response(1,{},'Error fetching data'));
-		 		}
-		 });
+		Product.fetchAll().then( Products => {
+			try {
+			resolve(new Response(1, Products.map(getProductData), "Successful Product Fecth"));
+			}
+			catch(err){
+			reject(new Response(0,{},'Error fetching data'));
+			}
+		});
 	});
-	return promise;
+return promise;
 }
+
+
+api.insertProducts = (data) => {
+	let response = {};
+	let promise  = new Promise( (resolve, reject) => {
+
+	Product.forge({
+		category_id: data.category_id,
+		name: 		 data.name,
+		cant: 		 data.cat,   
+		date:        data.date,
+		description: data.description,
+		price_min:   data.price_min ,  
+		price_max:   data.price_max
+	}).save().then( products => {
+		try {
+			resolve(new Response(1,data,"Successful Product Save"));
+		}
+		catch(err){
+			reject(new Response(0,{},'Error saving data'));
+		}
+		});
+	});
+return promise;
+}
+
+api.deleteProducts = (id) => {
+	let promise  = new Promise( (resolve, reject) => {
+		Product.where(id).destroy().then( Products => {
+			try {
+			resolve(new Response(1,id, "Successful Product Removed"));
+			}
+			catch(err){
+			reject(new Response(0,{},'Error removing data'));
+			}
+		});
+	});
+return promise;
+}
+
 
 module.exports = api;
